9de1d4a80a632c3aef6f31b9c0e3431e508e0155
badreddinne <badr-bencherki@live.fr>
2021-04-07 21:20:36 +0200

    feature/environmentVariable

--- helloWord.py
+++ helloWord.py
@@ -1,17 +1,31 @@
 import discord
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
-
+print(DISCORD_TOKEN)
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
+
+    print(message.author)
+    if "badr" in message.author.name:
+        await message.channel.send("badr has spoken")
+    if "Akurafu" in  message.author.name  :
+        await message.channel.send("Achraf u know that ur friends hate u :'(")
+    if "Kawa" in  message.author.name  :
+        await message.channel.send("o kawaii koto ~~")
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$'):
+        await message.channel.send(message.content)
+        print(message.content)
 
-client.run('ODI5MDQ4ODgwNDQzNTU1OTEw.YGyd8A.sGCyou4j4UhSid5aWxllXaOCj20')
+client.run(DISCORD_TOKEN)
